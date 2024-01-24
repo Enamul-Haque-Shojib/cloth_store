83d1d980b644c972c8f1602213ace183817bddeb
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2024-01-24 23:01:43 +0600

    css class added

--- cloth/urls.py
+++ cloth/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 router = DefaultRouter()
 
@@ -25,4 +26,8 @@ urlpatterns = [
     path('deletewishlistall/', views.deleteWishlistAll, name='delete_wishlist_all' ),
     path('buynowcartlist/', views.BuyNow, name='buynow_cartlist' ),
     
+    path('passwordreset/', auth_views.PasswordResetView.as_view(template_name="password_reset_form.html"), name = 'password_reset'),
+    path('passwordresetdone/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name = 'password_reset_done'),
+    path('passwordresetconfirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name = 'password_reset_confirm'),
+    path('passwordresetcomplete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name = 'password_reset_complete'),
 ]
