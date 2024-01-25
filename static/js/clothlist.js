b50b8e73a59789632cb173b3bc4b84ab51af9e42
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2024-01-26 05:34:39 +0600

    css class added

--- static/js/clothlist.js
+++ static/js/clothlist.js
@@ -97,29 +97,21 @@ const displayCategory = (categories) =>{
         const div = document.createElement("div");
         if (count_cat == 1){
             div.className = 'card card_small';
-            div.style.backgroundImage = `url('${category.image}')`;
-            div.innerHTML = `
-        <div onclick="searchItem('${category.name}')">${category.name}</div>
-        `; 
+            div.style.backgroundImage = `url('${category.image}')`; 
             count_cat++;
         }else if(count_cat==2){
             div.className = 'card card_medium';
             div.style.backgroundImage = `url('${category.image}')`;
-            div.innerHTML = `
-        <div onclick="searchItem('${category.name}')">${category.name}</div>
-        `; 
             count_cat++;
         }else if(count_cat==3){
             div.className = 'card card_large';
             div.style.backgroundImage = `url('${category.image}')`;
-            div.innerHTML = `
-        <div onclick="searchItem('${category.name}')">${category.name}</div>
-        `; 
-            count_cat++;
-        }else{
             count_cat=1;
         }
         
+        div.innerHTML = `
+        <div onclick="searchItem('${category.name}')">${category.name}</div>
+        `; 
          
          
         parent.appendChild(div);     
